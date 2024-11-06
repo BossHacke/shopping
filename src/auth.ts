39b5c751ce7e2d35a4b110070e294743574ecc8d
BossHacke <vonguyenphihung123@gmail.com>
2024-11-06 16:16:22 +0700

    login-nextjs

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import { InvalidAccountError, InvalidEmailPasswordError } from "./utils/errors";
+import { sendRequest } from "./utils/api";
+import { IUser } from "./types/next-auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
@@ -8,26 +11,62 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                 email: {},
                 password: {}
             },
-            authorize: async () => {
-                let user = null;
+            authorize: async (credentials) => {
 
-                user = {
-                    _id: "123",
-                    username: "123",
-                    email: "123",
-                    isVerify: "123",
-                    type: "123",
-                    role: "123"
+                const res = await sendRequest<IBackendRes<ILogin>>({
+                    method: "POST",
+                    url: "http://localhost:8000/api/auth/login",
+                    body: {
+                        username: credentials.email,
+                        password: credentials.password,
+                    }
+                });
+
+                console.log("check user", res);
+
+                if (!res.statusCode) {
+                    return {
+                        _id: res.data?.user._id,
+                        name: res.data?.user.name,
+                        email: res.data?.user.email,
+                        access_token: res.data?.user.access_token
+                    };
+                } else if (+res.statusCode === 401) {
+                    throw new InvalidEmailPasswordError();
+                } else if (+res.statusCode === 400) {
+                    throw new InvalidAccountError();
+                } else {
+                    throw new Error("Internal server error");
                 }
 
-                if (!user) {
-                    throw new Error("User not found.")
-                }
-                return user;
+                // user = {
+                //     _id: "123",
+                //     username: "123",
+                //     email: "123",
+                //     isVerify: "123",
+                //     type: "123",
+                //     role: "123"
+                // }
+
+                // if (!user) {
+                //     throw new InvalidEmailPasswordError();
+                // }
             }
         })
     ],
     pages: {
         signIn: "/auth/login"
+    },
+    callbacks: {
+        jwt({ token, user }) {
+            if (user) {
+                token.user = (user as IUser);
+            }
+            return token;
+        },
+        session({ session, token }) {
+            (session.user as IUser) = token.user;
+            return session;
+        }
     }
 })
--- src/utils/action.ts
+++ src/utils/action.ts
@@ -4,17 +4,28 @@ import { signIn } from "@/auth"
 export async function authenticate(email: string, password: string) {
     try {
         const r = await signIn("credentials", {
-            username: email,
+            email: email,
             password: password,
             // callbackUrl: "/dash_board",
             redirect: false
         })
         return r;
     } catch (error) {
-        // if (error.cause.err instanceof InvalidLoginError) {
-        //     return { "error": "Incorrect username or password" };
-        // } else {
-        //     throw new Error("failed to authenticate");
-        // }
+        if ((error as any).name === "InvalidEmailPasswordError") {
+            return {
+                error: (error as any).type,
+                code: 1
+            }
+        } else if ((error as any).name === "InvalidAccountError") {
+            return {
+                error: (error as any).type,
+                code: 2
+            }
+        } else {
+            return {
+                error: "Internal server error",
+                code: 0
+            }
+        }
     }
 }
